46f100cc83b4e9ad8e438b6eb79610cba117f255
Yong Chiang Pin <cpyong1996@gmail.com>
2022-02-26 19:11:48 +0800

    clean up route folder

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+// Entry point to server
 const express = require('express');
+// const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const path = require('path');
 
@@ -11,10 +13,10 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/posts', require('./routes/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
