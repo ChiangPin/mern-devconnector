48183969fb75d5526702e7dd72813b1024c56858
Yong Chiang Pin <cpyong1996@gmail.com>
2022-02-28 01:11:01 +0800

    credential error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,42 +30,36 @@ const authenticateUser = asyncHandler(async (req, res) => {
 
   const { email, password } = req.body;
 
-  try {
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (!user) {
-      return res
-        .status(400)
-        .json({ errors: [{ msg: 'Invalid Credentials' }] });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res
-        .status(400)
-        .json({ errors: [{ msg: 'Invalid Credentials' }] });
-    }
-
-    const payload = {
-      user: {
-        id: user.id
-      }
-    };
-
-    jwt.sign(
-      payload,
-      config.get('jwtSecret'),
-      { expiresIn: '5 days' },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
+  if (!user) {
+    res.status(400);
+    throw new Error('Invalid Credentials');
   }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    res.status(400);
+    throw new Error('Invalid Credentials');
+  }
+
+  const payload = {
+    user: {
+      id: user.id
+    }
+  };
+
+  jwt.sign(
+    payload,
+    config.get('jwtSecret'),
+    { expiresIn: '5 days' },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
+
 });
 
 module.exports = {
